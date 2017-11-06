34a0cb25777b14ec2109e31da487d180991fc2c2
Alex5Moon <alex521ace@gmail.com>
2017-11-06 19:33:29 +0800

    unsynchTest

---
+++ CoreJavaVolume-I/v1ch14/unsynch/Bank.java
@@ -0,0 +1,43 @@
+package v1ch14.unsynch;
+
+/**14-6
+ * A bank with a number of bank accounts.
+ * @author Administrator
+ *
+ */
+public class Bank 
+{
+	private final double[] accounts;
+	
+	public Bank(int n, double initialBalance)
+	{
+		accounts = new double[n];
+		for(int i=0; i<accounts.length; i++)
+			accounts[i] = initialBalance;
+	}
+	
+	public void transfer(int from, int to, double amount)
+	{
+		if(accounts[from]<amount) return;
+		System.out.print(Thread.currentThread());
+		accounts[from] -= amount;
+		System.out.printf(" %10.2f from %d to %d", amount, from, to);
+		accounts[to]   += amount;
+		System.out.printf(" Total Balance : %10.2f%n", getTotalBalance());
+	}
+	
+	public double getTotalBalance()
+	{
+		double sum = 0;
+		
+		for(double a : accounts)
+			sum += a;
+		
+		return sum;
+	}
+	
+	public int size()
+	{
+		return accounts.length;
+	}
+}
